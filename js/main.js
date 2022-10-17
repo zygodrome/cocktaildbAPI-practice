875bb27f50037d3ed1a24ba07fc09fc751e4f1b8
Jon Hernandez <zygodrome@gmail.com>
2022-10-17 13:20:16 -0500

    Got the page to rotate through the drinks it gets back every 15s

--- js/main.js
+++ js/main.js
@@ -8,22 +8,43 @@ fetch("https://www.thecocktaildb.com/api/json/v1/1/random.php")
     .catch(err => {
         console.log(`error ${err}`)
     });
+
 let search = function(){
     let cocktailQuery = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=` + document.getElementById("cocktailQuery").value;
 
     fetch(cocktailQuery)
     .then(res => res.json())
     .then(data => {
+        
         console.log(data)
         document.querySelector("h2").innerHTML = data.drinks[0].strDrink;
         document.querySelector("h3").innerHTML = data.drinks[0].strInstructions;
         document.querySelector("img").src = data.drinks[0].strDrinkThumb;
-        
+        carousel(data.drinks);
         
     })
     .catch(err => {
         console.log(`error ${err}`)
     });
 }
+
+
+let carousel = function(drinkArr){
+    
+    let count = 0;
+    setInterval(() => {
+        
+        document.querySelector("h2").innerHTML = drinkArr[count].strDrink;
+        document.querySelector("h3").innerHTML = drinkArr[count].strInstructions;
+        document.querySelector("img").src = drinkArr[count].strDrinkThumb;
+        count+=1;
+        if (count >= drinkArr.length){
+            count = 0;
+        }
+        
+    }, 15000);
+    
+}
+
 //PUSH HW IS TO TRY AND MAKE IT SWITCH BETWEEN ALL DRINKS RETURNED
 //NON PUSH IS TO MAKE IT WITHOUT A TEMPLATE LITERAL
